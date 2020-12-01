c0ee3e453c700411ad77d3b795cafaa09471f4fb
tubasa.takei <fhleben.dntsygdby@gmail.com>
2020-12-01 13:58:22 +0000

    task model

--- resources/views/tasks/show.blade.php
+++ resources/views/tasks/show.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 
-    <h1>id = {{ $task->id}}のタスク詳細ページ</h1>
+    <h1>id = {{ $task->id }}のタスク詳細ページ</h1>
     
     <table class="table table-bordered">
         <tr>
@@ -17,7 +17,7 @@
     
     {!! link_to_route('tasks.edit', 'このタスクを編集', ['task' => $task->id], ['class' => 'btn btn-light']) !!}
     
-    {!! Form::model($task, ['route' => ['tasks.destroy', $task->id], 'method' => 'delete') !!}
+    {!! Form::model($task, ['route' => ['tasks.destroy', $task->id], 'method' => 'delete']) !!}
         {!! Form::submit('削除', ['class' => 'btn btn-dager']) !!}
     {!! Form::close() !!}
 

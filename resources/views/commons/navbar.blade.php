cf9475b5dbf0e2146481782721171681e40c6ab0
tubasa.takei <fhleben.dntsygdby@gmail.com>
2020-12-12 12:31:56 +0000

    user registration

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -10,7 +10,8 @@
         <div class="collapse navbar-collapse" id="nav-bar">
             <ul class="navbar-nav mr-auto"></ul>
             <ul class="navbar-nav">
-                
+                <li class="nav-item">{!! link_to_route('signup.get', 'ユーザ登録', [], ['class' => 'nav-link']) !!}</li>
+                <li class="nav-item">{!! link_to_route('login', 'ログイン', [], ['class' => 'nav-link']) !!}</li>
                 <li class="nav-item">{!! link_to_route('tasks.create', '新規タスクの投稿',[], ['class' => 'nav-link']) !!}</li>
             </ul>
         </div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -78,6 +78,19 @@
                     @endauth
                 </div>
             @endif
+            
+            {{--　以下の位置でよいか後で確認する--}}
+            @extends('layouts.app')
+            
+            @section('content')
+                <div class="center jumbotron">
+                    <div class="text-center">
+                        <h1>welcome to the Tasklist</h1>
+                        {{-- ユーザ登録ページへのリンク　--}}
+                        {!! link_to_route('signup.get', 'Sign up now!', [], ['class' => 'btn btn-lg btn-primary']) !!}
+                    </div>
+                </div>
+            @endsection
 
             <div class="content">
                 <div class="title m-b-md">
